57544c53b6ac6b466facaef63421238e0fb8e6c7
ChanhoKim <chanhokim9848@gmail.com>
2024-06-16 18:50:35 +0100

    16/06/2024

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 const Navbar = () => {
+
+
+  const [menu,setMenu] = useState("home")
+
+
+
+
+
   return (
     <div className="navbar">
       {/* Navbar Logo Setting */}
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
         {/* Navbar Menu Setting */}
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile</li>
-        <li>Contact</li>
+        {/* when a menu is clicked and active, the menu shows its underline */}
+        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
+        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
+        <li onClick={()=>setMenu("mobile")} className={menu==="mobile"?"active":""}>Mobile</li>
+        <li onClick={()=>setMenu("contact")} className={menu==="contact"?"active":""}>Contact</li>
       </ul>
       {/* Navbar Right Setting */}
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
-        <div className="search-icon">
+        <div className="navbar-search-icon">
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+
+const Home = () => {
+  return (
+    <div>
+        {/* getting Header component on Home */}
+      <Header/>
+    </div>
+  )
+}
+
+export default Home
