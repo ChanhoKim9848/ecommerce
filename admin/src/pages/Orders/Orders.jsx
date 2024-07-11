6d9e0d78bcb7b515c6860b20e086f9df6ba12d3f
ChanhoKim <chanhokim9848@gmail.com>
2024-07-11 09:11:40 +0100

    Display Orders in Admin Panel - 11/07/24

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,12 +1,81 @@
-import React from 'react'
-import './Order.css'
+import React from "react";
+import "./Order.css";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { useEffect } from "react";
+import { assets } from "../../assets/assets";
 
-const Orders = () => {
+// Orders in Admin Panel
+const Orders = ({ url }) => {
+  const [orders, setOrders] = useState([]);
+
+  // Order list API
+  const fetchAllOrders = async () => {
+    const response = await axios.get(url + "/api/order/list");
+    if (response.data.success) {
+      setOrders(response.data.data);
+      console.log(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, []);
+
+  // User order display
   return (
-    <div>
-      orders
+    <div className="order add">
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {orders.map((order, index) => (
+          <div key={index} className="order-item">
+            <img src={assets.parcel_icon} alt="" />
+            <div>
+              <p className="order-item-item">
+                {/*  Order list of the user */}
+                {order.items.map((item, index) => {
+                  if (index === order.items.length - 1) {
+                    return item.name + " x " + item.quantity;
+                  } else {
+                    return item.name + " x " + item.quantity + ", ";
+                  }
+                })}
+              </p>
+              {/* User name of the orders */}
+              <p className="order-item-name">
+                {order.address.firstName + " " + order.address.lastName}
+              </p>
+              {/* User address of the orders */}
+              <div className="order-item-address">
+                <p>{"Address: "+ order.address.street + ","}</p>
+                <p>
+                  {order.address.city +
+                    ", " +
+                    order.address.state +
+                    ", " +
+                    order.address.country +
+                    ", " +
+                    order.address.zipcode}
+                </p>
+              </div>
+              <p className="order-item-phone">{"Contact: "+ order.address.phone}</p>
+            </div>
+            <p>Items : {order.items.length}</p>
+            <p>£{order.amount}</p>
+            <select>
+              {/* Order Processing in Admin Panel */}
+              <option value="Item Processing">Item Processing</option>
+              <option value="Out for Delivery">Out for Delivery</option>
+              <option value="Delivered">Delivered</option>
+            </select>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Orders
+export default Orders;
--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,6 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order from frontend
 const placeOrder = async (req, res) => {
-  
   const frontend_url = "http://localhost:5173";
 
   try {
@@ -86,4 +85,16 @@ const userOrders = async (req, res) => {
   }
 };
 
-export { placeOrder, verifyOrder, userOrders };
+
+// Listing Orders for Admin Panel
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { placeOrder, verifyOrder, userOrders, listOrders };
